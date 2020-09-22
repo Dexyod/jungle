689d296388116c732b8faede748799ddcd90b9cd
Dexyod <dexyod43@gmail.com>
2020-09-22 16:50:11 +0000

    testing for add to cart complete

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,12 +17,10 @@ RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
     end
   end
 
-  scenario "when clicking on a product" do
+  scenario 'when clicking on add button' do
     visit root_path
-    save_screenshot('main_page_before_single_product.png')
-    find(:css, '.product:first-child header a').click
-    expect(page).to have_css('.product-detail')
-    save_screenshot('single_product.png')
+    find(:css, '.product:first-child footer form button').click
+    expect(page).to have_content " My Cart (1)"
+    save_screenshot('cart_test.png')
   end
-
 end
